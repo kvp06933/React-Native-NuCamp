13da58b0350a0253a0cb95c060b383b32be1c9b0
Katherine <kvp06933@gmail.com>
2020-05-27 15:44:37 -0500

    Navigation Part 2

--- nucampsite/components/MainComponent.js
+++ nucampsite/components/MainComponent.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
-        Directory: {screen: Directory},
-        CampsiteInfo: {screen: CampsiteInfo}
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
     },
     {
         initialRouteName: 'Directory',
@@ -23,14 +24,44 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
-class Main extends Component {
-        render() {
-            return (
-            <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <DirectoryNavigator />
-            </View>
-            );
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
         }
     }
+);
 
-    export default Main;
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
+class Main extends Component {
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+            }}>
+                <MainNavigator />
+            </View>
+        );
+    }
+}
+
+export default Main;
